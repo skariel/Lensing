a67db55d296b92dd8ef388890ea93e2cd468178c
Ariel Keselman <skariel@gmail.com>
2016-09-30 16:27:46 +0300

    more LSS

--- src/time.jl
+++ src/time.jl
@@ -1,12 +1,15 @@
 
-function time(massfunc, x, y)
+function time(phifunc, x, y)
     dx = x[2:end] - x[1:(end-1)]
     dy = y[2:end] - y[1:(end-1)]
     dpath = sqrt(dx.*dx + dy.*dy)
-    r = sqrt(x.*x + y.*y)
-    rm = 0.5*(r[2:end] + r[1:(end-1)])
-    m = [massfunc(ri) for ri in rm]
-    time_dilation = sqrt(1.0-2.*G.*m./rm./C./C)
+    phi = zeros(length(dx))
+    for i in 1:length(dx)
+        xc = x[i] + 0.5*dx[i] 
+        yc = y[i] + 0.5*dy[i] 
+        phi[i] = phifunc(xc,yc)
+    end
+    time_dilation = sqrt(1.0-2.*phi)
     sum(dpath./time_dilation) ./ C
 end
 
